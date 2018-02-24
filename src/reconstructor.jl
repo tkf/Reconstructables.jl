68075dafe54c749ecea1179c8d839e76b7d76753
Takafumi Arakaki <aka.tkf@gmail.com>
2018-02-23 17:58:21 -0800

    Generate recon for (theoretical) type-stability
    From this discussion
    https://discourse.julialang.org/t/ann-setfield-jl-yet-another-package-for-working-with-immutables/9267/4
    I realized that using struct_as_dict at run-time may also induce
    type-instability.  But this can be avoided by generated function.

--- src/reconstructor.jl
+++ src/reconstructor.jl
@@ -2,15 +2,16 @@ constructor_of(::T) where T = constructor_of(T)
 constructor_of(T::Type) = getfield(T.name.module, Symbol(T.name.name))
 constructor_of(T::UnionAll) = constructor_of(T.body)
 
-struct_as_dict(st) = [(n => getfield(st, n)) for n in fieldnames(typeof(st))]
-
 """
     recon(thing; <keyword arguments>)
 
 Re-construct `thing` with new field values specified by the keyword
 arguments.
 """
-function recon(thing; kwargs...)
-    constructor = constructor_of(thing)
-    return constructor(; struct_as_dict(thing)..., kwargs...)
+@generated function recon(thing; kwargs...)
+    fields = [Expr(:kw, k, :(thing.$k)) for k in fieldnames(thing)]
+    return quote
+        constructor = $constructor_of(thing)
+        constructor(; $(fields...), kwargs...)
+    end
 end
